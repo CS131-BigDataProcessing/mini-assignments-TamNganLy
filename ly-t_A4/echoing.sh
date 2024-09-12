9820d24ee5f2b49a7ee27d2c635f51b857955500
TamNganLy <tamly1810@gmail.com>
2024-09-12 02:40:58 -0400

    Tam Ly added to A4

--- ly-t_A4/echoing.sh
+++ ly-t_A4/echoing.sh
@@ -20,7 +20,7 @@ mathvar2=$[mathvar1*20]
 mathvar3=10
 mathvar4=$[$mathvar1*($mathvar2+$mathvar3)]
 echo "Activity 3"
-echo "Varibale 1 is $((mathvar1)). Variable 2 is $((mathvar2)). Using $mathvar3 for Variable 3, our final Variable 4 is $((mathvar4))."
+echo "Varibale 1 is $mathvar1. Variable 2 is $mathvar2. Using $mathvar3 for Variable 3, our final Variable 4 is $mathvar4."
 printf "\n"
 
 # Activity 4
