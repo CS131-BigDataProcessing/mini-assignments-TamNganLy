056a65bf6c5a598208202a0ea9f3b1ad9d5e457a
TamNganLy <tamly1810@gmail.com>
2024-09-16 12:54:03 -0400

    Tam Ly added to A5

---
+++ ly-t_A5/age.sh
@@ -0,0 +1,10 @@
+age=$1
+if [ $age -lt 13 ]; then
+	echo "child"
+elif [ $age -lt 20 ]; then
+	echo "teen"
+elif [ $age -lt 65 ]; then
+	echo "adult"
+else
+	echo "elderlly"
+fi
---
+++ ly-t_A5/temperature.sh
@@ -0,0 +1,15 @@
+temp=$1
+
+if [ $temp -gt 55 ]; then
+	if [ $temp -lt 67 ]; then
+		echo "cold"
+	else
+		if [ $temp -lt 85 ]; then
+			echo "nice"
+		else
+			echo "hot"
+		fi
+	fi
+else
+	echo "freezing"
+fi
