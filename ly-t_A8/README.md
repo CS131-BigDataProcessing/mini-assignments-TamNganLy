90be0fcb9b6b79e0bb0b5aa3ea478fac0a0c3ef8
TamNganLy <tamly1810@gmail.com>
2024-11-16 19:52:19 -0500

    Updated A8

--- ly-t_A8/README.md
+++ ly-t_A8/README.md
@@ -1,2 +1,16 @@
-Calling the shell script follow by the file name, number of column, and the column that you want to handle outlier.
-Exp: sh preprocess.sh AB_NYC_2019.csv 16 10
+# A8 Directory  
+
+## Files  
+- **preprocessing.sh**: A shell script that automates the cleaning and preprocessing of data. It performs tasks: removing missing value, removing duplicate entries, and handling outliers.  
+- **about_script.txt**: A document explaining the reasoning behind the tasks implemented in `preprocessing.sh` and listing the names of the group members.  
+
+## How to Run  
+
+Run the script using the following syntax:  
+```bash  
+sh preprocessing.sh <filename> <number_of_columns> <outlier_column>  
+```
+Example:
+```bash 
+sh preprocessing.sh AB_NYC_2019.csv 16 10  
+```
--- ly-t_A8/preprocessing.sh
+++ ly-t_A8/preprocessing.sh
@@ -4,48 +4,48 @@ file_name=$1
 num_of_col=$2
 outlier_col=$3
 
+# Task 1: Identify missing values
+
 # Handle trailing spaces/new lines.
 awk -F, '{
-    gsub(/^[[:space:]]+|[[:space:]]+$/, "", $0); # Remove leading/trailing spaces
+    gsub(/^[[:space:]]+|[[:space:]]+$/, "", $0);
     gsub(/\t/, "", $0); # Remove tabs
     gsub(/\n/, "", $0); # Remove newlines
     print
 }' "$file_name" > input_data.csv
 
-# Handle fields with commas inside quotes using FPAT to treat them as one field
-#awk -v FPAT='([^,]+)|(\"[^\"]+\")' '
-#{
-#    for (i=1; i<=NF; i++) {
-#        gsub(/^"|"$/, "", $i);
-#        printf "%s%s", $i, (i<NF ? OFS : ORS); # Output field
-#    }
-#}' OFS=',' temp_cleaned_data.csv > input_data.csv
-
-
 # Identify missing values
 awk -F, -v col=$2 'NF==col' input_data.csv > input_data1.csv
 
+
+
+# Task 2: Remove rows with missing values
+
 # If there are missing values, loop through each column and remove rows with missing values
-awk -F',' -v OFS=',' -v col="$outlier_col" '
-	{
+awk -F',' -v OFS=',' '
+{
 	missing = 0;
 	for (i=1; i<=NF; i++) {
-        	if ($i == "") {missing = 1; break}
+		if ($i == "") {missing = 1; break}
     	}
 	if (missing == 0) { print $0 };
 }' input_data1.csv > clean_data.csv
 
-# Remove duplicate
+
+
+# Task 3:  Remove duplicate
+
 # Extract the header and store it temporarily
 head -n 1 clean_data.csv > unique_data.csv
 
 # Sort and remove duplicates from the remaining rows, then append to the file
 tail -n +2 clean_data.csv | sort -t',' -k 1,1 | uniq >> unique_data.csv
 
-# sort -t',' -k 1,1 clean_data.csv | uniq > unique_data.csv
 
-# Handle outliers
-# Select the column with the outliers
+
+# Task 4: Identifying and handling outliers
+
+# Select the column that need to hande outliers
 cut -d',' -f$outlier_col unique_data.csv | sort -nu > field_values.txt
 
 # Calculate Q1, Q2, Q3
@@ -59,7 +59,6 @@ echo $Q1
 echo $Q2
 echo $Q3
 
-
 # Calculate the IQR and determine the bounds for outliers
 IQR=$(echo "$Q3 - $Q1" | bc)
 lower_bound=$(echo "$Q1 - 1.5 * $IQR" | bc)
@@ -69,7 +68,7 @@ echo $lower_bound
 echo $upper_bound
 
 
-# Step 3: Replace outliers with Q2 (Median)
+# Replace outliers with Q2 (Median)
 awk -F',' -v q2="$Q2" -v lb="$lower_bound" -v ub="$upper_bound" -v col="$outlier_col" '
 {
     if ($col < lb || $col > ub) {
